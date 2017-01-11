53bfa5431cafc06d8329f310c046071515d79a24
Kyle Harding <kylemharding@gmail.com>
2017-01-11 13:31:45 -0500

    skip duplicate file uploads

--- README.md
+++ README.md
@@ -16,14 +16,14 @@ docker build -t megasync ~/.docker/images/megasync
 #### run ####
 ```bash
 docker run \
-    --rm
-    --privileged
+    --rm \
+    --privileged \
     --name=megasync \
     -v path/to/config:/config \
     -v path/to/snapshots/target:/snapshots \
-    -e HOSTNAME=$HOSTNAME \
-    -e MEGAUSER=<mega username> \
-    -e MEGAPASS=<mega password> \
+    -e HOSTNAME="$(hostname)" \
+    -e MEGAUSER="your_username" \
+    -e MEGAPASS="your_password" \
     megasync
 ```
 
@@ -31,11 +31,16 @@ docker run \
 #### build ####
 ```bash
 git clone git@github.com:klutchell/docker-megasync.git ~/.docker/images/megasync
-HOSTNAME=$HOSTNAME docker-compose -f ~/.docker/images/megasync/megasync.yml build megasync
+docker-compose -f ~/.docker/images/megasync/megasync.yml build megasync
 ```
 #### run ####
 ```bash
-HOSTNAME=$HOSTNAME docker-compose -f ~/.docker/images/megasync/megasync.yml run --rm megasync
+docker-compose -f ~/.docker/images/megasync/megasync.yml run \
+    --rm \
+    -e HOSTNAME="$(hostname)" \
+    -e MEGAUSER="your_username" \
+    -e MEGAPASS="your_password" \
+    megasync
 ```
 
 ## Contributing ##
--- start.sh
+++ start.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
+# check for existing config
 if [ -f /config/megafuse.conf ]; then
 	echo "using external configuration file"
 elif [[ $MEGAUSER ]] && [[ $MEGAPASS ]]; then
-	echo "using default configuration fil with provided username and password"
-	sed -e "s|_MEGAUSER_|$MEGAUSER|" -e "s|_MEGAPASS_|$MEGAPASS|" /root/megafuse.conf /config/megafuse.conf
+	echo "using default configuration with provided username and password"
+	sed -e "s|_MEGAUSER_|${MEGAPASS}|" -e "s|_MEGAPASS_|${MEGAPASS}|" /root/megafuse.conf > /config/megafuse.conf
 else
 	echo "no credentials provided"; exit 1
 fi
 
+# set snapshot location
+snapshot="/snapshots/alpha.0/localhost/source"
+[ -d "${snapshot}" ] || { echo "snapshot not found"; exit 1; }
+
+# set destination path based on hostname and date
+[ -n "${HOSTNAME}" ] || { echo "hostname cannot be blank"; exit 1; }
+echo "hostname is ${HOSTNAME}"
+dest="/data/snapshots/${HOSTNAME}"
+tarball="$(date -r "${snapshot}" +%Y.%m.%d_%H.%M.%S).tar.gz"
+
+# start megafuse in the background and output to log
 echo "starting megafuse..."
 /opt/megafuse/MegaFuse -c "/config/megafuse.conf" &>/config/megafuse.log &
 
+# wait up to a minute for mount to complete
 counter=0
 while ! grep -q 'user received or updated' /config/megafuse.log && [ "${counter}" -lt 60 ]
 do
@@ -19,22 +32,28 @@ do
 	let counter=counter+1
 done
 
+# proceed if the mount appears valid
 if mountpoint -q "/data" && [ "${counter}" -lt 60 ]
 then
 	echo "megafuse is ready"
-	mkdir "/data/snapshots" 2>/dev/null || true 
-	[ -d "/snapshots/alpha.0" ] || { echo "snapshot not found"; exit 1; }
-	pushd "/snapshots/alpha.0" >/dev/null
+	mkdir "${dest}" 2>/dev/null || true 
+	
+	# check if already exists
+	[ -f "${dest}/${tarball}" ] && { echo "snapshot already exists on remote"; exit 0; }
+	
+	# compress
+	pushd "${snapshot}" >/dev/null
 	echo "compressing snapshot..."
-	tar -czf "/data/snapshots/$(hostname)_$(date -r "/snapshots/alpha.0" +%Y.%m.%d_%H.%M.%S).tar.gz" *
+	tar -czf "${dest}/${tarball}" *
 	popd >/dev/null
 else
 	echo "megafuse failed or not ready"; exit 1
 fi
 
+# ls on the destination will force the cached upload to complete
 echo "uploading snapshot..."
-ls -alt "/data/snapshots/$(hostname)_"*
-# 'upload riuscito'
+ls -lt "${dest}"/
 
-echo "deleting oldest snapshots..."
-ls -t "/data/snapshots/$(hostname)_"* | sed -e '1,3d' | xargs -d '\n' rm -v
+# delete oldest snapshot if more than 3 exist
+echo "removing old snapshots..."
+ls -t "${dest}"/ | sed -e '1,3d' | xargs -d '\n' rm -v
